406be2949499397169a3039f13553afe4514cbd0
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-08 13:57:12 +0800

    I copied and paste some codes in from the earlier program that will take the number from the user. And the class color for it.

---
+++ number_addition_operator.py
@@ -0,0 +1,20 @@
+class color:
+    purple = '\033[95m'
+    red = '\033[91m'
+    yellow = '\033[93m'
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+    end = '\033[0m'
+    green = '\033[92m'
+
+while True:
+    try:
+        input_first_number = float(input(f'{color.yellow + color.bold}ENTER FIRST NUMBER -->{color.end} '))
+        input_second_number = float(input(f'{color.yellow + color.bold}ENTER SECOND NUMBER -->{color.end} '))
+        
+        
+        
+    except ValueError as e:
+        print(f'{color.red + color.bold}Input an appropriate value. Please try again.{color.end}')
