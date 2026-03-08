4ca0db236def0bcdc89e9cbf679f7d8f6b1df5fb
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-08 13:29:45 +0800

    I added the conditions for the program to determine the bigger number between the two numbers.

--- bigger_number_operator.py
+++ bigger_number_operator.py
@@ -1,6 +1,19 @@
 while True:
     try:
-    
+        input_first_number = float(input("ENTER FIRST NUMBER --> "))
+        input_second_number = float(input("ENTER SECOND NUMBER --> "))
+        
+        if input_first_number > input_second_number:
+            print(f'{input_first_number} is greater than {input_second_number}')
+            break
+        
+        elif input_first_number == input_second_number:
+            print(f'{input_first_number} is equal to {input_second_number}')
+            break
+        
+        else:
+            print(f'{input_second_number} is greater than {input_first_number}')
+            break
     
     except ValueError as e:
         print("Input an appropriate value. Please try again.")
