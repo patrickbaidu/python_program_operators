0313402cec3d24a5d69e27f6ac4f977018a16ddd
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-11 01:41:39 +0800

    I added the operations for the program using the len function and modulo to find the odd number.

--- odd_number_counter.py
+++ odd_number_counter.py
@@ -11,23 +11,30 @@ class color:
 
 try:
     
+    number_list = []
     odd_number_list = []
     
     for user_input in range(1,11):
         if user_input == 1:
             input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}ST NUMBER --> {color.end}' ))
-            odd_number_list.append(input_number)
+            number_list.append(input_number)
         elif user_input == 2:
             input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}ND NUMBER --> {color.end}'))
-            odd_number_list.append(input_number)
+            number_list.append(input_number)
         elif user_input == 3:
             input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}RD NUMBER --> {color.end}'))
-            odd_number_list.append(input_number)
+            number_list.append(input_number)
         else:
             input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}TH NUMBER --> {color.end}'))
-            odd_number_list.append(input_number)
+            number_list.append(input_number)
     
+    for number in number_list:
+        if number % 2 == 1:
+            odd_number_list.append(number)
     
+    count_odd_number = f'{len(odd_number_list)} Odd Numbers'
+    
+    print(f'There are {color.green + color.bold}{count_odd_number}{color.end} in the number list.')
     
 except ValueError as e:
     print(f'{color.red + color.bold}Input an appropriate value. Please try again.{color.end}')
