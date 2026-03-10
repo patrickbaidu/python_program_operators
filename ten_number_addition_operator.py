104fcc2cf29938a718fd1cd5cec38914ee7d8f13
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-11 01:32:08 +0800

    I finished the program by adding the operations needed for it. I also used lists and increment operator to find the sum and display it.

--- ten_number_addition_operator.py
+++ ten_number_addition_operator.py
@@ -16,26 +16,29 @@ try:
     
     for user_input in range(1,11):
         if user_input == 1:
-            input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}ST NUMBER --> '))
+            input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}ST NUMBER --> {color.end}' ))
             increment_of_input_number += input_number
             input_number_list.append(input_number)
         elif user_input == 2:
-            input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}ND NUMBER --> '))
+            input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}ND NUMBER --> {color.end}'))
             increment_of_input_number += input_number
             input_number_list.append(input_number)
         elif user_input == 3:
-            input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}RD NUMBER --> '))
+            input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}RD NUMBER --> {color.end}'))
             increment_of_input_number += input_number
             input_number_list.append(input_number)
         else:
-            input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}TH NUMBER --> '))
+            input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}TH NUMBER --> {color.end}'))
             increment_of_input_number += input_number
             input_number_list.append(input_number)
     
-    summation_of_numbers = f'{color.bold}{input_first_number}{color.end}'
+    summation_of_numbers = f'{color.green + color.bold}{increment_of_input_number}{color.end}'
     
-    input_number_list_bold = f'{color.bold}{input_number_list}{color.end}'
-
+    equation_of_list = " + ".join(str(number) for number in input_number_list)
+    equation_of_list = f'{color.cyan + color.bold}{equation_of_list}{color.end}'
+    
+    print(f'Sum of {equation_of_list} =')
+    print(summation_of_numbers)
 
     
 except ValueError as e:
