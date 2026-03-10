d760d42727730cf5ae5ffd22b0dfa3a9e1c59cc8
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-11 01:07:43 +0800

    I added the operators for the program to function.

--- ten_number_addition_operator.py
+++ ten_number_addition_operator.py
@@ -11,15 +11,31 @@ class color:
 
 try:
     
+    increment_of_input_number = 0
+    input_number_list = []
+    
     for user_input in range(1,11):
         if user_input == 1:
             input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}ST NUMBER --> '))
+            increment_of_input_number += input_number
+            input_number_list.append(input_number)
         elif user_input == 2:
             input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}ND NUMBER --> '))
+            increment_of_input_number += input_number
+            input_number_list.append(input_number)
         elif user_input == 3:
             input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}RD NUMBER --> '))
+            increment_of_input_number += input_number
+            input_number_list.append(input_number)
         else:
             input_number = float(input(f'{color.yellow + color.bold} ENTER {user_input}TH NUMBER --> '))
+            increment_of_input_number += input_number
+            input_number_list.append(input_number)
+    
+    summation_of_numbers = f'{color.bold}{input_first_number}{color.end}'
+    
+    input_number_list_bold = f'{color.bold}{input_number_list}{color.end}'
+
 
     
 except ValueError as e:
