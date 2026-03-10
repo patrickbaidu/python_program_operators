f4eb13b9778667a4a4da23dd55a8e500d4ab7836
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-11 01:34:38 +0800

    I copy and pasted the color class and try block.

---
+++ odd_number_counter.py
@@ -0,0 +1,26 @@
+class color:
+    purple = '\033[95m'
+    red = '\033[91m'
+    yellow = '\033[93m'
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+    end = '\033[0m'
+    green = '\033[92m'
+
+try:
+    input_first_number = float(input(f'{color.yellow + color.bold}ENTER FIRST NUMBER -->{color.end} '))
+    input_second_number = float(input(f'{color.yellow + color.bold}ENTER SECOND NUMBER -->{color.end} '))
+    
+    quotient_of_numbers = input_first_number / input_second_number
+    
+    first_number_bold = f'{color.bold}{input_first_number}{color.end}'
+    second_number_bold = f'{color.bold}{input_second_number}{color.end}'
+    quotient_of_numbers_colored = f'{color.green}{quotient_of_numbers}{color.end}'
+    
+    print(f'{first_number_bold} ÷ {second_number_bold} = {quotient_of_numbers_colored}')
+    print(f'{quotient_of_numbers_colored} is the quotient of the two numbers.')
+    
+except ValueError as e:
+    print(f'{color.red + color.bold}Input an appropriate value. Please try again.{color.end}')
