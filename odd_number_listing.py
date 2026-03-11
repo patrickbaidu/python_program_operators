75ebd091bf5dcc6e9cd6207a3ec4ef30b8b47d6c
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-12 00:42:45 +0800

    I deleted unecessary code lines, and changed some code because of logic errors.

--- odd_number_listing.py
+++ odd_number_listing.py
@@ -9,21 +9,17 @@ class color:
     end = '\033[0m'
     green = '\033[92m'
 
-try:
-    print(f'{color.yellow + color.bold}{'-'*4} LISTS OF EVEN NUMBERS FROM 0-100 {'-'*4}{color.end}')
-    
-    number = 100
-    odd_number_list = []
-    
-    while number > 0:
-        number -= 1
-        if number % 2 == 1:
-            odd_number_list.append(number)
-    
-    list_of_odd_numbers = ', '.join(str(number) for number in odd_number_list)
-    list_of_odd_numbers = f'{color.bold}{list_of_odd_numbers}{color.end}'
-    
-    print(list_of_odd_numbers)
+print(f'{color.yellow + color.bold}{'-'*4} LIST OF ODD NUMBERS FROM 0-100 {'-'*4}{color.end}')
 
-except ValueError as e:
-    print(f'{color.red + color.bold}Input an appropriate value. Please try again.{color.end}')
+number = 0
+odd_number_list = []
+
+while number < 100:
+    number += 1
+    if number % 2 == 1:
+        odd_number_list.append(number)
+
+list_of_odd_numbers = ', '.join(str(number) for number in odd_number_list)
+list_of_odd_numbers = f'{color.bold}{list_of_odd_numbers}{color.end}'
+
+print(list_of_odd_numbers)
