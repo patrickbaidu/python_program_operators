ad656e2c0d57a8614230de8cebb85c67c4d83018
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-12 01:29:40 +0800

    I added the condition where when there is no in-between numbers, it will retry the program for another input. I also used the reverse function for the bigger first number and smaller second number so that it matches the result.

--- number_range_operator.py
+++ number_range_operator.py
@@ -20,31 +20,44 @@ while True:
         input_second_number_increment = input_second_number + 1
         
         if input_first_number > input_second_number:
+            if input_second_number_increment == input_first_number:
+                print(f'{color.red}There are no in-between numbers, Please Try Again{color.end}')
             
-            for between_number in range(input_second_number_increment, input_first_number):
-                between_number_list.append(between_number)
+            else:
+                for between_number in range(input_second_number_increment, input_first_number):
+                    between_number_list.append(between_number)
                 
-            number_list = ", ".join(str(number) for number in between_number_list)
-            number_list = f'{color.bold}{number_list}{color.end}'
-            
-            input_first_number_colored = f'{color.purple + color.bold}{input_first_number}{color.end}'
-            input_second_number_colored = f'{color.purple + color.bold}{input_second_number}{color.end}'
-            
-            print(f'The numbers between {input_first_number_colored} and {input_second_number_colored} are {number_list}')
+                between_number_list.reverse()
+                number_list = ", ".join(str(number) for number in between_number_list)
+                number_list = f'{color.bold}{number_list}{color.end}'
+                
+                input_first_number_colored = f'{color.purple + color.bold}{input_first_number}{color.end}'
+                input_second_number_colored = f'{color.purple + color.bold}{input_second_number}{color.end}'
+                
+                print(f'The numbers between {input_first_number_colored} and {input_second_number_colored} = {number_list}')
+                
+                break
         
+        elif input_first_number < input_second_number:
+            if input_first_number_increment == input_second_number:
+                print(f'{color.red}There are no in-between numbers, Please Try Again{color.end}')
+            
+            else:
+                for between_number in range(input_first_number_increment, input_second_number):
+                    between_number_list.append(between_number)
+                    
+                number_list = ", ".join(str(number) for number in between_number_list)
+                number_list = f'{color.bold}{number_list}{color.end}'
+                
+                input_first_number_colored = f'{color.purple + color.bold}{input_first_number}{color.end}'
+                input_second_number_colored = f'{color.purple + color.bold}{input_second_number}{color.end}'
+                
+                print(f'The numbers between {input_first_number_colored} and {input_second_number_colored} = {number_list}')
+                
+                break
+            
         else:
-            for between_number in range(input_first_number_increment, input_second_number):
-                between_number_list.append(between_number)
-                
-            number_list = ", ".join(str(number) for number in between_number_list)
-            number_list = f'{color.bold}{number_list}{color.end}'
+            print(f'{color.red}There are no in-between numbers, Please Try Again{color.end}')
             
-            input_first_number_colored = f'{color.purple + color.bold}{input_first_number}{color.end}'
-            input_second_number_colored = f'{color.purple + color.bold}{input_second_number}{color.end}'
-            
-            print(f'The numbers between {input_first_number_colored} and {input_second_number_colored} are {number_list}')
-            
-        break
-        
     except ValueError as e:
         print(f'{color.red + color.bold}Input an appropriate value. Please try again.{color.end}')
