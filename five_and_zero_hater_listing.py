2ed4a32167b0fba2d586f1574663a8250561334b
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-12 00:52:50 +0800

    I copied and pasted the code from zero hater listing then added a condition for numbers that ends with five.

---
+++ five_and_zero_hater_listing.py
@@ -0,0 +1,17 @@
+class color:
+    purple = '\033[95m'
+    red = '\033[91m'
+    yellow = '\033[93m'
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+    end = '\033[0m'
+    green = '\033[92m'
+
+print(f'{color.yellow + color.bold}{'-'*4} LIST OF NUMBERS FROM 0-100 THAT DOES NOT END IN ZEROES AND FIVES {'-'*4}{color.end}')
+
+for number in range(0, 101):
+    if number % 5 != 0 and number % 10 != 0:
+        number_bold = f'{color.bold}{number}{color.end}'
+        print(number_bold, end=", ")
