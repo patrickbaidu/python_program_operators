589fc38078a3275376c971af1979a08c2da00342
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-12 23:43:46 +0800

    I copied and pasted the code from the unique number detector then configured it to condition the sorted list to print the index of 0 since the lowest number in the sorted list will be displayed in the terminal once the user input a new number to the system.

---
+++ lowest_number_detector.py
@@ -0,0 +1,25 @@
+class color:
+    purple = '\033[95m'
+    red = '\033[91m'
+    yellow = '\033[93m'
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+    end = '\033[0m'
+    green = '\033[92m'
+
+input_number_list = []
+    
+while True:
+    try:
+        user_input_number = int(input(f'{color.yellow + color.bold}ENTER A NUMBER --> {color.end}'))
+
+        input_number_list.append(user_input_number)
+        sorted_number_list = sorted(input_number_list)
+        print(f'Lowest Number: {color.purple + color.bold}{sorted_number_list[0]}{color.end}')
+
+    except ValueError as e:
+        print(f'{color.red + color.bold}Input an appropriate value. {color.end}')
+        break
+
