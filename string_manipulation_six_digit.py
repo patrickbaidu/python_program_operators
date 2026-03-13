0f23f1959af304078b4c8072d44cc011a311aab6
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-13 10:51:16 +0800

    I added the while loop, I addded the operations and condiitons.

--- string_manipulation_six_digit.py
+++ string_manipulation_six_digit.py
@@ -9,7 +9,25 @@ class color:
     end = '\033[0m'
     green = '\033[92m'
 
-try:
+zero_digit = 0
 
-except ValueError as e:
-    print(f'{color.red + color.bold}Input an appropriate value. Please try again.{color.end}')
+while True:
+    try:
+        user_input = int(input(f'{color.yellow + color.bold}ENTER FROM 0-1000 --> {color.end}'))
+        
+        if user_input > 1000 or user_input < 0:
+            raise ValueError
+
+        else:
+            user_input = str(user_input)
+            for number in user_input:
+                zero_digit += 1
+            
+            zero_amount = 6 - zero_digit
+            final_digits = f'{'0' * zero_amount}' + user_input
+        
+        print(f'{color.green + color.bold}{final_digits}{color.end}')
+        break
+    
+    except ValueError as e:
+        print(f'{color.red + color.bold}Input an appropriate value. Please try again.{color.end}')
