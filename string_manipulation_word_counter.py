1b7c46108119457e128668de74f6c12adf9a4c97
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-13 11:12:51 +0800

    I copied and pasted the code from the previous code then used the split function to split the strings, then used the len function to count the datas in the list.

--- string_manipulation_word_counter.py
+++ string_manipulation_word_counter.py
@@ -9,7 +9,18 @@ class color:
     end = '\033[0m'
     green = '\033[92m'
 
-try:
+while True:
+    try:
+        user_input = input(f'{color.yellow + color.bold}ENTER FULL NAME --> {color.end}')
+        
+        if not user_input.replace(' ', '').isalpha():
+            raise ValueError
 
-except ValueError as e:
-    print(f'{color.red + color.bold}Input an appropriate value. Please try again.{color.end}')
+    except ValueError as e:
+        print(f'{color.red + color.bold}Input an appropriate value. Please try again.{color.end}')
+    else:
+        user_input = user_input.strip()
+        user_input = user_input.split()
+        word_count = len(user_input)
+        print(f'WORD COUNT: {color.green + color.bold}{word_count}{color.end}')
+        break
