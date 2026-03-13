51ffc729ee0288cceb810461c0f49652ccddc192
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-03-13 09:52:56 +0800

    I copied and pasted the template block.

---
+++ string_manipulation_front_space.py
@@ -0,0 +1,10 @@
+class color:
+    purple = '\033[95m'
+    red = '\033[91m'
+    yellow = '\033[93m'
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+    end = '\033[0m'
+    green = '\033[92m'
